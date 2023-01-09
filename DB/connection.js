c339edb99d4caac28cf793e6aa78cb63b87e1431
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-09 15:52:49 +0500

    Git Init

--- DB/connection.js
+++
@@ -1,9 +0,0 @@
-import { connect } from "mongoose";
-
-connect("mongodb+srv://admin:admin1234@cluster0.jivsgsq.mongodb.net/userdata?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-}).then(() => {
-    console.log('Connection is Established');
-}).catch((err) => {
-    console.log('Error: ', err);
-})
